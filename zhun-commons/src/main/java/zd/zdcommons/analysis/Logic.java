d41c7bd3c0d0e8116bd548dbc327d4df15024942
chenkun <569353189@qq.com>
2019-06-06 15:15:47 +0800

    修改Message和返回类型

--- zhun-commons/src/main/java/zd/zdcommons/analysis/Logic.java
+++ zhun-commons/src/main/java/zd/zdcommons/analysis/Logic.java
@@ -1,9 +1,11 @@
 package zd.zdcommons.analysis;
 
 import org.apache.commons.lang3.StringUtils;
+import zd.zdcommons.pojo.Message;
 import zd.zdcommons.serviceImp.AnalysisImp;
 import zd.zdcommons.pojo.ResultMessage;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import static zd.zdcommons.Utils.importByExcelForDate;
 
@@ -11,6 +13,8 @@ public class Logic implements AnalysisImp {
     @Override
     public ResultMessage getIntegrityAnalysis(Map<String, Object> resource,Map<String,Object> titleMap) {
         ResultMessage resultm = null;
+        Message message = null;
+        List<Message> meslist=new ArrayList<Message>();
         ArrayList<String> list = new ArrayList<String>();
         //记录count数
         long count=0;
@@ -18,12 +22,13 @@ public class Logic implements AnalysisImp {
         for (String str :logicStr){
             String mes = resource.get(str).toString();
             if(StringUtils.isBlank(mes)||mes.equals("N/A")){
-
                 list.add(titleMap.get(str)+"为空");
             }
         }
         if(list.size()>0){
-            resultm=getMessge(resultm,resource,list,count);
+            message = get5G(message, list);
+            meslist.add(message);
+            resultm=getMessge(resultm,resource,meslist,count);
             return resultm;
         }
 
@@ -38,52 +43,43 @@ public class Logic implements AnalysisImp {
             }
         }
         if(list.size()>0){
-            resultm=getMessge(resultm,resource,list,count);
+            message = get5G(message, list);
+            meslist.add(message);
+            resultm=getMessge(resultm,resource,meslist,count);
         }
         return resultm;
     }
 
     private final static String [] logicStr={"YD5-RFI-actualEndDate","YD5-MOS-actualEndDate","YD5-IC-actualEndDate","YD5-SC-actualEndDate"};
-    public static ResultMessage getIntegrityLogicAnalysize(Map<String, Object> resource){
-        ResultMessage resultm = null;
-        ArrayList<String> list = new ArrayList<String>();
-        //记录count数
-        long count=0;
-        //获取数据
-        for (String str :logicStr){
-            String mes = resource.get(str).toString();
-            if(StringUtils.isBlank(mes)||mes.equals("N/A")){
-
-                list.add(mes+"为空");
-            }
-        }
-        if(list.size()>0){
-            resultm=getMessge(resultm,resource,list,count);
-            return resultm;
-        }
-
-        for (int i=0;i<logicStr.length-1;i++){
-            int qian = Integer.parseInt(resource.get(logicStr[i]).toString());
-            String qianTime = importByExcelForDate(resource.get(logicStr[i]).toString());
-            int hou = Integer.parseInt(resource.get(logicStr[i+1]).toString());
-            String houTime = importByExcelForDate(resource.get(logicStr[i+1]).toString());
-            if(qian<hou){
-                list.add(logicStr[i]+": "+qianTime+"  | 大于 |"+logicStr[i+1]+"："+houTime);
-            }
-        }
-        if(list.size()>0){
-            resultm=getMessge(resultm,resource,list,count);
-        }
-        return resultm;
-    }
-    private final static ResultMessage getMessge(ResultMessage resultm,Map<String, Object> resource,ArrayList<String> list,long count){
+    private final static ResultMessage getMessge(ResultMessage resultm, Map<String, Object> resource, List<Message> mes, long count){
         resultm=new ResultMessage();
         resultm.setTError("逻辑性错误");
         resultm.setDUID(resource.get("YD5-dUID").toString());
         resultm.setDUName(resource.get("YD5-dUName").toString());
         resultm.setDarea(resource.get("YD5-area").toString());
-        resultm.setMessge(list);
+        resultm.setMessge(mes);
         resultm.setXcount(count);
         return resultm;
     };
+    private final static Message get5G(Message mes,List<String> list){
+        mes = new Message();
+        mes.setAction("G");
+        mes.setTitle("5G");
+        mes.setMessages(list);
+        return mes;
+    }
+    private final static Message get3DMIMO(Message mes,List<String> list){
+        mes = new Message();
+        mes.setAction("D");
+        mes.setTitle("3D-MIMO");
+        mes.setMessages(list);
+        return mes;
+    }
+    private final static Message get1800Anchor(Message mes,List<String> list){
+        mes = new Message();
+        mes.setAction("M");
+        mes.setTitle("锚点1800M");
+        mes.setMessages(list);
+        return mes;
+    }
 }

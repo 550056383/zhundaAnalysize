f5330266fa8fc94e00a5a037f87289ead352f7a1
chenkun <569353189@qq.com>
2019-06-11 17:41:11 +0800

    修改方法

--- zhun-commons/src/main/java/zd/zdcommons/analysis/Logic.java
+++ zhun-commons/src/main/java/zd/zdcommons/analysis/Logic.java
@@ -25,26 +25,48 @@ public class Logic implements AnalysisImp {
         ArrayList<String> list = new ArrayList<String>();
         //记录count数
         long count=0;
+        //判断开关
+        boolean lot=false;
+        //获取校验下标
+        int index=-1;
         //获取数据
-        for (String str :logicStr){
-            String mes = resource.get(str).toString();
-            if(StringUtils.isBlank(mes)||mes.equals("N/A")){
-                list.add(titleMap.get(str)+"为空");
+        System.out.println("进入logic");
+        for(int i= logicStr.length-1;i>=0;i--){
+            String mes = resource.get(logicStr[i]).toString();
+            if (lot){
+                if (StringUtils.isBlank(mes)||mes.equals("N/A")){
+                    list.add(titleMap.get(logicStr[i])+"为空");
+                }
+            }
+            if(!StringUtils.isBlank(mes)&&!mes.equals("N/A")){
+                if(lot==false){
+                    lot=true;
+                    index=i;
+                }
             }
         }
+//        for (String str :logicStr){
+//            String mes = resource.get(str).toString();
+//            if(StringUtils.isBlank(mes)||mes.equals("N/A")){
+//                list.add(titleMap.get(str)+"为空");
+//            }
+//        }
         if(list.size()>0){
+
             message = get5G(message, list);
             meslist.add(message);
             resultm=getMessge(resultm,resource,meslist,count);
             return resultm;
+
         }
-
-        for (int i=0;i<logicStr.length-1;i++){
-
-            int qian = Integer.parseInt(resource.get(logicStr[i]).toString());
-            String qianTime = utils.importByExcelForDate(resource.get(logicStr[i]).toString());
-            int hou = Integer.parseInt(resource.get(logicStr[i+1]).toString());
-            String houTime = utils.importByExcelForDate(resource.get(logicStr[i+1]).toString());
+        if(index<2) {
+            return resultm;
+        }
+        for (int i=index-1;i>0;i--){
+            int qian = Integer.parseInt(resource.get(logicStr[i-1]).toString());
+            String qianTime = utils.importByExcelForDate(resource.get(logicStr[i-1]).toString());
+            int hou = Integer.parseInt(resource.get(logicStr[i]).toString());
+            String houTime = utils.importByExcelForDate(resource.get(logicStr[i]).toString());
             if(qian>hou){
                 list.add(titleMap.get(logicStr[i])+": "+qianTime+"  | 大于 |"+titleMap.get(logicStr[i+1])+"："+houTime);
             }

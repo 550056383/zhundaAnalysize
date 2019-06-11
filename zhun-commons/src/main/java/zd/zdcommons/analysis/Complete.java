cce8b834fd2c2fbdbb51ecffa0f971b8032b5010
chenkun <569353189@qq.com>
2019-06-11 09:29:22 +0800

    修改字段

--- zhun-commons/src/main/java/zd/zdcommons/analysis/Complete.java
+++ zhun-commons/src/main/java/zd/zdcommons/analysis/Complete.java
@@ -192,39 +192,14 @@ public class Complete implements AnalysisImp {
             "FDD1800规划编号","FDD1800施工计划","锚点FDD1800问题分类","FDD1800是否规划","FDD1800到货日期",
             "FDD1800交优完成日期","FDD1800安装","FDD1800开通","FDD1800 NM NE ID","FDD1800网管基站名称"};
     //数组
-    private static String shu [] ={"YD5-customerSiteID" ,"YD5-customerSiteName","YD5-dUID	YD5-dUName","YD5-area",
-            "YD5-Subcontractor","YD5-supervisionUnit","YD5-SC-baselineStartDate","YD5-SC-baselineEndDate",
-            "YD5-SC-planStartDate","YD5-SC-planEndDate","YD5-SC-actualStartDate","YD5-SC-actualEndDate","YD5-SC-owner",
-            "YD5-SC-approveState","YD5-SC-deliveryAttachmentRequired","YD5-SC-deliveryAttachmentUploaded",
-            "YD5-SC-accumulation","YD5-SC-remarks","YD5-SC-delayReason","YD5-AAU-baselineStartDate","YD5-AAU-baselineEndDate",
-            "YD5-AAU-planStartDate","YD5-AAU-planEndDate","YD5-AAU-actualStartDate","YD5-AAU-actualEndDate","YD5-AAU-owner",
-            "YD5-AAU-approveState","YD5-AAU-deliveryAttachmentRequired","YD5-AAU-deliveryAttachmentUploaded",
-            "YD5-AAU-accumulation","YD5-AAU-remarks","YD5-AAU-delayReason","YD5-residentialBroadband",
-            "YD5-transmissionEquipped","YD5-deliveryType","YD5-Nmneid","YD5-nROPO","YD5-rruToneNumber","YD5-rruHardwareNumber",
-            "YD5-spectrum","YD5-nroServiceContract","YD5-scenario","YD5-auuArrivalQuantity","YD5-problemClassification",
-            "YD5-RFI-baselineEndDate","YD5-RFI-planEndDate","YD5-RFI-actualEndDate","YD5-RFI-owner","YD5-RFI-approveState",
-            "YD5-RFI-deliveryAttachmentRequired","YD5-RFI-deliveryAttachmentUploaded","YD5-RFI-accumulation",
-            "YD5-RFI-remarks","YD5-RFI-delayReason","YD5-MOS-baselineEndDate","YD5-MOS-planEndDate","YD5-MOS-actualEndDate",
-            "YD5-MOS-owner","YD5-MOS-approveState",	"YD5-MOS-deliveryAttachmentRequired",
-            "YD5-MOS-deliveryAttachmentUploaded","YD5-MOS-accumulation","YD5-MOS-remarks","YD5-MOS-delayReason",
-            "YD5-completionDate",
-            "YD5-receptionDate","YD5-IC-baselineEndDate","YD5-IC-planEndDate","YD5-IC-actualEndDate","YD5-IC-owner"
-            ,"YD5-IC-approveState","YD5-IC-deliveryAttachmentRequired",	"YD5-IC-deliveryAttachmentUploaded",
-            "YD5-IC-accumulation","YD5-IC-remarks",	"YD5-IC-delayReason","YD5-engineeringServiceMode","YD5-planningNumber",
-            "YD5-tianmianTransformation",	"YD5-dcFuse",	"YD5-acInduction","YD5-design","YD5-bbuToneNumber","YD5-bbuESN",
-            "YD5-bbuSiteID","YD5-bbuSiteName","YD5-bbuScenario","YD5-bbuHardwareNumber",	"YD5-deliveryRegion","YD5-nmNEName",
-            "YD5-rruSiteID","YD5-rruScenario","YD5-rruBoxNo",
-            "YD5-standingType","YD5-productModel","YD5-contractConnection","YD5-remoteStationType","YD5-standard",
-            "YD5-transmissionBandwidth","YD5-nroSubcontractor","YD5-standingType2","MIMO-miMO3DDate","MIMO-miMO3DID",
-            "MIMO-miMO3DGoodsQuantity","MIMO-deliveryDate",
-            "MIMO-questionClassification","MIMO-planningNumber","MIMO-installationDate","MIMO-completionDate",
-            "MIMO-nmNEID","MIMO-openTypeStand","MIMO-openTypeStandTarget","MIMO-baseStationName","MIMO-transmissionBandwidthe4G",
-            "MIMO-transmissionAvailable4G",
-            "M1800-deliveryDateFDD","M1800-programNumberFDD","M1800-constructionPlanFDD","M1800-questionClassificationFDD",
-            "M1800-whetherPlanningFDD",
-           "M1800-arrivalDateFDD","M1800-deliveryCompletionDateFFD","M1800-installationFDD",
-            "M1800-openedFDD","M1800-nmNEIDFDD","M1800-baseStationNameFDD"
-    };
+    private static String shu [] ={"YD5-customerSiteID","YD5-customerSiteName","YD5-dUID","YD5-dUName","YD5-area","YD5-Subcontractor","YD5-supervisionUnit","YD5-SC-baselineStartDate","YD5-SC-baselineEndDate","YD5-SC-planStartDate","YD5-SC-planEndDate","YD5-SC-actualStartDate","YD5-SC-actualEndDate","YD5-SC-owner","YD5-SC-approveState","YD5-SC-deliveryAttachmentRequired","YD5-SC-deliveryAttachmentUploaded","YD5-SC-accumulation","YD5-SC-remarks","YD5-SC-delayReason","YD5-AAU-baselineStartDate",
+            "YD5-AAU-baselineEndDate","YD5-AAU-planStartDate","YD5-AAU-planEndDate","YD5-AAU-actualStartDate","YD5-AAU-actualEndDate","YD5-AAU-owner","YD5-AAU-approveState","YD5-AAU-deliveryAttachmentRequired","YD5-AAU-deliveryAttachmentUploaded","YD5-AAU-accumulation","YD5-AAU-remarks","YD5-AAU-delayReason","YD5-residentialBroadband","YD5-transmissionEquipped","YD5-deliveryType","YD5-Nmneid","YD5-nROPO","YD5-rruToneNumber","YD5-rruHardwareNumber","YD5-spectrum","YD5-nroServiceContract",
+            "YD5-scenario","YD5-auuArrivalQuantity","YD5-problemClassification","YD5-RFI-baselineEndDate","YD5-RFI-planEndDate","YD5-RFI-actualEndDate","YD5-RFI-owner","YD5-RFI-approveState","YD5-RFI-deliveryAttachmentRequired","YD5-RFI-deliveryAttachmentUploaded","YD5-RFI-accumulation","YD5-RFI-remarks","YD5-RFI-delayReason","YD5-MOS-baselineEndDate","YD5-MOS-planEndDate","YD5-MOS-actualEndDate","YD5-MOS-owner","YD5-MOS-approveState","YD5-MOS-deliveryAttachmentRequired","YD5-MOS-deliveryAttachmentUploaded",
+            "YD5-MOS-accumulation","YD5-MOS-remarks","YD5-MOS-delayReason","YD5-completionDate","YD5-receptionDate","YD5-IC-baselineEndDate","YD5-IC-planEndDate","YD5-IC-actualEndDate","YD5-IC-owner","YD5-IC-approveState","YD5-IC-deliveryAttachmentRequired","YD5-IC-deliveryAttachmentUploaded","YD5-IC-accumulation","YD5-IC-remarks","YD5-IC-delayReason","YD5-engineeringServiceMode","YD5-planningNumber","YD5-tianmianTransformation","YD5-dcFuse","YD5-acInduction","YD5-design","YD5-bbuToneNumber",
+            "YD5-bbuESN","YD5-bbuSiteID","YD5-bbuSiteName","YD5-bbuScenario","YD5-bbuHardwareNumber","YD5-deliveryRegion","YD5-nmNEName","YD5-rruSiteID","YD5-rruSiteName","YD5-rruScenario","YD5-rruBoxNo","YD5-standingType","YD5-productModel","YD5-contractConnection","YD5-remoteStationType","YD5-standard","YD5-transmissionBandwidth","YD5-nroSubcontractor","YD5-standingType2","MIMO-miMO3DDate","MIMO-miMO3DID","MIMO-miMO3DGoodsQuantity","MIMO-deliveryDate","MIMO-questionClassification",
+            "MIMO-planningNumber","MIMO-installationDate","MIMO-completionDate","MIMO-nmNEID","MIMO-openTypeStand","MIMO-openTypeStandTarget","MIMO-baseStationName","MIMO-transmissionBandwidthe4G","MIMO-transmissionAvailable4G","M1800-deliveryDateFDD","M1800-programNumberFDD","M1800-constructionPlanFDD","M1800-questionClassificationFDD","M1800-whetherPlanningFDD","M1800-arrivalDateFDD","M1800-deliveryCompletionDateFFD","M1800-installationFDD","M1800-openedFDD","M1800-nmNEIDFDD","M1800-baseStationNameFDD"};
+
+
     //第一个规则所需要判断的字段
     private static final String[] duidstr = {"YD5-dUID","YD5-customerSiteID","YD5-customerSiteName",
             "YD5-dUName","YD5-area","YD5-Subcontractor","YD5-spectrum","YD5-nroServiceContract",

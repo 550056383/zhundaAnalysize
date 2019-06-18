accffe7917dfb82c9b407e1cfdc426e2eb61d662
chenkun <569353189@qq.com>
2019-06-18 11:18:09 +0800

    添加工程

--- zhunda-analysis/src/main/java/zd/zdanalysis/web/EnginnerController.java
+++ zhunda-analysis/src/main/java/zd/zdanalysis/web/EnginnerController.java
@@ -1,5 +1,6 @@
 package zd.zdanalysis.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import zd.zdanalysis.service.EnginnerService;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/enginner")
 public class EnginnerController {
+    @Autowired
     private EnginnerService enginnerService;
-    @PutMapping("enginner")
-    public ResponseEntity<String> getTitle(@RequestParam("file") MultipartFile file, String sheetName, int index){
-        return ResponseEntity.ok(enginnerService.getMessage());
+    @PutMapping("title")
+    public ResponseEntity<List<String>> getTitle(@RequestParam("file") MultipartFile file){
+        System.out.println("jinlai");
+        return ResponseEntity.ok(enginnerService.getMessage(file));
     };
 }
